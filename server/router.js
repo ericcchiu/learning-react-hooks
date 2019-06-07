3a360e546ca45ad4b50822cde8a7c20bf2ce2d76
EXC1Q3X <eric_chiu@homedepot.com>
2019-06-07 14:07:58 -0500

    (FEATURE) Add /todolist route and todos route

--- server/router.js
+++ server/router.js
@@ -8,13 +8,25 @@ router.get('/', (req, res) => {
   res.send('Welcome to the react boiler plate');
 });
 
-router.post('/savetodo', async(req, res) => {
+/* GET/ todos : Retrieve a list of todos */
+router.get('/todolist', async(req, res) => {
+  try {
+    let response = await axios.get('https://todolist-ebce0.firebaseio.com/todos.json');
+    res.status(200).send(await response.data);
+  } catch(error) {
+    res.status(404).send(error);
+  }
+});
+
+/* POST/ todos : Save a single todo */ 
+router.post('/todo', async(req, res) => {
   // console.log('content: ', req.body)
   // res.send(req.body)
   try {
     await axios.post('https://todolist-ebce0.firebaseio.com/todos.json', req.body)
+    res.status(201).send('Todo Created');
   } catch(error) {
-    res.status(201).send('Error saving todo');
+    res.status(400).send('Error saving todo');
   }
 });
 
