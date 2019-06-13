f20852ae32034061b3aeacce42bbd6a337512a06
EXC1Q3X <eric_chiu@homedepot.com>
2019-06-13 14:45:13 -0500

    (FORMAT) Format code to abide by style guide

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,56 +1,55 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 const Todo = props => {
-  const [todoName, setTodoName] = useState('');
+  const [todoName, setTodoName] = useState("");
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
-    const asyncFetchTodos = async() => {
+    const asyncFetchTodos = async () => {
       try {
-        const todoResponse = await axios.get('/todolist');
+        const todoResponse = await axios.get("/todolist");
         const todosData = await todoResponse.data;
         const todos = [];
 
-        for(let key in todosData) {
-          todos.push({id: key, name: todosData[key].name});
+        for (let key in todosData) {
+          todos.push({ id: key, name: todosData[key].name });
         }
         setTodoList(todos);
-      } catch(error) {
+      } catch (error) {
         console.error(error);
       }
     };
     asyncFetchTodos();
     return () => {
-      console.log('Cleanup');
+      console.log("Cleanup");
     };
   }, []);
-  
+
   const mouseMoveHandler = event => {
     console.log(event.clientX, event.clientY);
-  }
+  };
 
   useEffect(() => {
-    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener("mousemove", mouseMoveHandler);
     return () => {
-      document.removeEventListener('mousemove', mouseMoveHandler );
-    }
+      document.removeEventListener("mousemove", mouseMoveHandler);
+    };
   });
 
-
   const inputChangeHandler = event => {
-    setTodoName(event.target.value)
+    setTodoName(event.target.value);
   };
 
   const todoAddHandler = () => {
     setTodoList(todoList.concat(todoName));
-    const  postToFirebase = async() => {
+    const postToFirebase = async () => {
       try {
-        await axios.post('/todo', {name: todoName});
+        await axios.post("/todo", { name: todoName });
       } catch (error) {
-        console.log('Error saving todo to database', error);
+        console.log("Error saving todo to database", error);
       }
-    }
+    };
     postToFirebase();
   };
 
