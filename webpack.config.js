ce90d1b0c1a19ff775a66849ae1ad0bf89ac40c5
EXC1Q3X <eric_chiu@homedepot.com>
2019-06-13 13:23:41 -0500

    (SETUP) Add transform runtime plugin to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,46 @@
 module.exports = {
   context: `${__dirname}/client/src`,
-  entry: './index.js',
+  entry: "./index.js",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          },
-        },
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-transform-runtime"
+            ]
+          }
+        }
       },
       {
         test: /\.css$/,
         loader: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader'
-          },
-        ],
+            loader: "css-loader"
+          }
+        ]
       },
       {
         test: /\.(png|jpg|gif)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192
             }
           }
         ]
       }
-    ],
+    ]
   },
   output: {
-    filename: 'root.bundle.js',
-    path: `${__dirname}/public`,
-  },
+    filename: "root.bundle.js",
+    path: `${__dirname}/public`
+  }
 };
