b6a9222b3197d2d0e43af4af5798e898d935bf6b
EXC1Q3X <eric_chiu@homedepot.com>
2019-06-13 13:20:25 -0500

    (CLEANUP) Remove @babel/polyfill import and reconfigure webpack instead.

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios'
-import '@babel/polyfill';
 
 const Todo = props => {
   const [todoName, setTodoName] = useState('');
