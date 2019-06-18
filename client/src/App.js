23a0c7689de8f14040294154325a894aaeec1fa8
EXC1Q3X <eric_chiu@homedepot.com>
2019-06-18 14:05:29 -0500

    (FEATURE) Add useContext hook to application for loginand authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
+
 import Todo from "./components/Todo";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import AuthContext from "./auth-context";
 
 const App = () => {
-  const [page, setPage] = useState('Auth');
+  const [page, setPage] = useState("Auth");
+  const [authStatus, setAuthStatus] = useState(false);
 
-  const switchPage = (pageName) => {
+  const switchPage = pageName => {
     setPage(pageName);
-  }
+  };
 
+  const login = () => {
+    setAuthStatus(true);
+  };
 
   return (
-    <div>
-      <Header 
-        onLoadTodos={switchPage.bind(this, 'Todos')} 
-        onLoadAuth={switchPage.bind(this, 'Auth')}
-      />
-      <hr />
-      {page === 'Auth' ? <Auth />: <Todo />}
-      
-      
-    </div>
+    <AuthContext.Provider value={{ status: authStatus, login: login }}>
+      <div className="App">
+        <Header
+          onLoadTodos={switchPage.bind(this, "Todos")}
+          onLoadAuth={switchPage.bind(this, "Auth")}
+        />
+        <hr />
+        {page === "Auth" ? <Auth /> : <Todo />}
+      </div>
+    </AuthContext.Provider>
   );
 };
 export default App;
