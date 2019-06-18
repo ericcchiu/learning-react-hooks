45eacd793d3a7e4bf64bc2cd295a81ea7e4d5bb0
EXC1Q3X <eric_chiu@homedepot.com>
2019-06-18 14:07:53 -0500

    (FEATURE) Add login button to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+import AuthContext from "../auth-context";
 
 const Header = props => {
+  const auth = useContext(AuthContext);
   return (
     <header>
-      <button onClick={props.onLoadTodos}>TodoList</button> {' | '}
+      {auth.status ? (
+        <button onClick={props.onLoadTodos}>TodoList</button>
+      ) : null}
       <button onClick={props.onLoadAuth}>Auth</button>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
-
-
